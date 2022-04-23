2f4b8132a9ec22f3e253b3b03de012acbf7ca049
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-04-23 12:54:05 +0200

    Annotations corrected

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,10 +1,13 @@
 package pl.coderslab.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.coderslab.dao.BookDao;
 import pl.coderslab.model.Book;
 
+@Controller
 public class BookController {
 
     private  final BookDao bookDao;
@@ -12,13 +15,13 @@ public class BookController {
         this.bookDao = bookDao;
     }
 
-    @RequestMapping("/book/add")
+    @GetMapping("/book/add")
     @ResponseBody
     public String hello() {
         Book book = new Book();
         book.setTitle("Thinking in Java");
         book.setAuthor("Bruce Eckel");
         bookDao.saveBook(book);
-        return "Id dodanej książki to " + book.getId();
+        return "id of added book: " + book.getId();
     }
 }
