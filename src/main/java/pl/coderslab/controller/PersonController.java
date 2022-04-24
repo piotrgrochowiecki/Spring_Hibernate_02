c7ef27f170792cb015d04f7fff77cb9dc54a9810
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-04-24 12:35:05 +0200

    Uzupełnienie PersonDao

--- src/main/java/pl/coderslab/controller/PersonController.java
+++ src/main/java/pl/coderslab/controller/PersonController.java
@@ -1,14 +1,19 @@
 package pl.coderslab.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pl.coderslab.dao.PersonDao;
 import pl.coderslab.model.Person;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor //zamiast konstruktora dla PersonDao
 public class PersonController {
+
+    private final PersonDao personDao;
 
     @GetMapping("person")
     public String personForm(Model model) {
@@ -20,6 +25,7 @@ public class PersonController {
     @ResponseBody
     public String handlePersonForm(@ModelAttribute("person") Person person) {
         log.info("New person {}", person);
+        personDao.save(person);
         return "ok";
     }
 
--- src/main/java/pl/coderslab/model/Person.java
+++ src/main/java/pl/coderslab/model/Person.java
@@ -2,9 +2,16 @@ package pl.coderslab.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "persons")
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String login;
     private String password;
     private String email;
