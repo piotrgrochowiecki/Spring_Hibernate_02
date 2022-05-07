56d85643cc0127403c1005d61cae38b4b877b6e1
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-05-07 09:57:19 +0200

    Dzień 3. Kolejne konfiguracje

--- src/main/java/pl/coderslab/AppConfig.java
+++ src/main/java/pl/coderslab/AppConfig.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+import java.util.Locale;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab")
@@ -40,4 +46,17 @@ public class AppConfig implements WebMvcConfigurer {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+
+    @Bean(name="localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl","PL"));
+        return localeResolver;
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    } //koniecznie import z javax.validation (nie javax.xml.validation)
+
 }
