d275c9c408ec5e599c0ed59f7947220239d1b977
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2022-05-07 11:01:19 +0200

    Dzień 3. Wprowadzenie do walidacji. Zadanie 4

--- src/main/java/pl/coderslab/model/Author.java
+++ src/main/java/pl/coderslab/model/Author.java
@@ -2,8 +2,10 @@ package pl.coderslab.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.pl.PESEL;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -20,8 +22,17 @@ public class Author {
     @Column(name = "first_name")
     private String firstName;
 
+    @NotNull
     @Column(name = "last_name")
     private String lastName;
+
+    @PESEL
+    @Column(name = "pesel")
+    private long pesel;
+
+    @Email
+    @Column(name = "email")
+    private String email;
 
     @Transient
     public String getFullName() {
